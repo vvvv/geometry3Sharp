d02f5a1b9f4731ecb91574aa4af9a35cd8ece062
Greg Meess <greg@sutromachine.com>
2020-06-17 16:13:18 -0700

    Add PolyLineCombiner class with initial implementation using DGraph2

---
+++ geometry3Sharp.Tests/curve/PolyLineCombiner.Tests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace g3.curve.UnitTests
+{
+    [TestClass]
+    public class PolyLineCombinerTests
+    {
+        private readonly double delta = 1e-6;
+
+        [TestMethod]
+        public void PolyLineCombiner_ConnectedSimple()
+        {
+            // Arrange
+            var polylines = new List<PolyLine2d>();
+            polylines.Add(new PolyLine2d(new Vector2d[] {
+                new Vector2d(1, 0),
+                new Vector2d(1, 1),
+            }));
+
+            polylines.Add(new PolyLine2d(new Vector2d[] {
+                new Vector2d(1, 0),
+                new Vector2d(1, -1),
+            }));
+
+            // Act
+            var result = PolyLineCombiner.CombineConnectedPolyLines(polylines, 1e-6);
+
+            // Assert
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(3, result[0].VertexCount);
+            Assert.AreEqual(2, result[0].ArcLength, delta);
+        }
+
+        [TestMethod]
+        public void PolyLineCombiner_UnconnectedSimple()
+        {
+            // Arrange
+            var polylines = new List<PolyLine2d>();
+            polylines.Add(new PolyLine2d(new Vector2d[] {
+                new Vector2d(1, 0.1),
+                new Vector2d(1, 1),
+            }));
+
+            polylines.Add(new PolyLine2d(new Vector2d[] {
+                new Vector2d(1, 0),
+                new Vector2d(1, -1),
+            }));
+
+            // Act
+            var result = PolyLineCombiner.CombineConnectedPolyLines(polylines, 1e-6);
+
+            // Assert
+            Assert.AreEqual(2, result.Count);
+        }
+
+    }
+}
---
+++ geometry3Sharp/curve/PolyLineCombiner.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace g3
+{
+    public static class PolyLineCombiner
+    {
+        public static List<PolyLine2d> CombineConnectedPolyLines(IEnumerable<PolyLine2d> polylines, double mergeThreshold)
+        {
+            var graph = new DGraph2();
+            foreach (var polyline in polylines)
+                MergeAppendPolyline(graph, polyline, mergeThreshold);
+            var curves = DGraph2Util.ExtractCurves(graph);
+            return curves.Paths;
+        }
+
+        private static int MergeAppendVertex(DGraph2 graph, Vector2d vertex, double mergeThreshold)
+        {
+            for (int i = 0; i < graph.VertexCount; i++)
+            {
+                double d = vertex.Distance(graph.GetVertex(i));
+                if (d < mergeThreshold)
+                    return i;
+            }
+
+            return graph.AppendVertex(vertex);
+        }
+
+        private static void MergeAppendPolyline(DGraph2 graph, PolyLine2d polyline, double mergeThreshold, int gid = -1)
+        {
+            int previousVertexIndex = -1;
+            foreach (var vertex in polyline)
+            {
+                int currentVertexIndex = MergeAppendVertex(graph, vertex, mergeThreshold);
+                if (previousVertexIndex != -1)
+                {
+                    graph.AppendEdge(previousVertexIndex, currentVertexIndex, gid);
+                }
+                previousVertexIndex = currentVertexIndex;
+            }
+        }
+    }
+}
