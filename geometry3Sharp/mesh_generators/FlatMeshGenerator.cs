8cfe31d490a36ef5fa9efcf67adc46da2aeeb888
ravazquez <ravazquez@gmail.com>
2022-08-26 16:54:54 -0600

    Add FlatMeshGenerator and TextureSpace enum

---
+++ geometry3Sharp/mesh_generators/FlatMeshGenerator.cs
@@ -0,0 +1,14 @@
+﻿namespace g3
+{
+    public abstract class FlatMeshGenerator : MeshGenerator
+    {
+        public bool GenerateBackFace = false;
+        public TextureSpace TextureSpace = TextureSpace.OpenGL;
+    }
+
+    public enum TextureSpace
+    {
+        DirectX,
+        OpenGL
+    }
+}
